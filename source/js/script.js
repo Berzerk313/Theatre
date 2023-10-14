95d3b365e84b739e1b87b60b8f8c6fc148f0fb7e
Your Name <you@example.com>
2023-10-14 16:59:07 +0300

    main swiper

--- source/js/script.js
+++ source/js/script.js
@@ -1,35 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let swiper = new Swiper(".swiper", {
+  let swiper = new Swiper(".main-swiper", {
     spaceBetween: 30,
-
+    effect: "cards",
+    grabCursor: true,
     centerSlides: 'true',
     fade: 'true',
-    loop: true,
-    autoplay: {
-      delay: 1000,
-    },
     breakpoints: {
-      375: {
+      1920: {
         slidesPerView: 1.1,
         spaceBetween: 10,
-        centerSlides: 'true',
       },
-      768: {
-        slidesPerView: 1.5,
-        spaceBetween: 5,
-      },
-      1110: {
-        slidesPerView: 2.1,
-        spaceBetween: 5,
-      },
-      1440: {
-        slidesPerView: 2.6,
-        spaceBetween: 5,
-      },
-      1600: {
-        slidesPerView: 3,
-        spaceBetween: 35,
-      },
+    },
+    navigation: {
+      nextEl: ".main-content-right__swipe-right",
+      prevEl: ".main-content-right__swipe-left",
     },
   });
 });
